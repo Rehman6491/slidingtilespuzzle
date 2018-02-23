6f3422f834d91d68f2a0584591fa7eb253f20bfc
Tran Minh Tuan <tranminhtuan684@gmail.com>
2018-02-23 15:49:44 +0700

    Sliding Tiles Puzzle

---
+++ slidingtiles.c
@@ -0,0 +1,41 @@
+/**
+ * slidingtiles.c
+ * 
+ * Implements the puzzle of sliding tiles of 3 x 3
+ * 
+ * Usage: slidingtiles
+ * 
+ * Using Hill-Climbing Search Algorithms
+ */
+
+#include <stdio.h>
+
+// constants
+#define true 1
+#define false 0
+
+// prototype
+
+// move tile and return true, else return false
+int move(int tile);
+
+// decide whether final state is reached
+int won(void); 
+
+int main(void)
+{
+    printf("Hello, World\n");
+    
+    // success
+    return 0;
+}
+
+int move(int tile)
+{
+    return true;
+}
+
+int won(void)
+{
+    return true;
+}
